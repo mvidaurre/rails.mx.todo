5bfcbd224d69588892431c86a747fb8d24e186d3
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2010-08-28 07:55:09 -0500

    User model first cut

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :lists
+  has_many :items, :through => :lists
+  validates_presence_of     :name, :message => "es requerido"
+  validates_uniqueness_of   :email, :message => "debe de ser único"
+  validates_format_of       :email, :with => /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = List.new(:name => "User Cucumber", :email => "user@cukes.example.com")
+  end
+  it "is valid with valid attributes" do
+    @user.should be_valid
+  end
+  it "is not valid without a name" do
+    @user.name = nil
+    @user.should_not be_valid
+  end
+  it "is not valid without a email" do
+    @user.email = nil
+    @user.should_not be_valid
+  end
+  it "is not valid without a proper email" do
+    @user.email = "email"
+    @user.should_not be_valid
+  end
 end
