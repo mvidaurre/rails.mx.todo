f1d6572d72089d6e8832ef2eee3a1fe378b8a7a6
Manuel Vidaurre <manuel.vidaurre@agiltec.com.mx>
2010-08-28 00:07:35 -0500

    Initial commit using BDD tools

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMxTodo::Application.config.secret_token = 'e03e2cf2c83244df6c0625cbfdc8ecebe9de789fc4db63f938ad125fe331616e4b63fd9b2b0c428bdc8aa4a349d6b42d8ff089efa8e681f92b14e0cd48ee4b9a'
